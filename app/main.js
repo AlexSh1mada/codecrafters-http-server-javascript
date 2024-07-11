acd0316441e6d76b27a0dda219e9cb090ce759ae
AlexSh1mada <alexbuerom@gmail.com>
2024-07-11 05:35:49 -0600

    lol

--- app/main.js
+++ app/main.js
@@ -5,28 +5,33 @@ console.log("Logs from your program will appear here!!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-    socket.on("data", (data) => {
-		const request = data.toString();
+	socket.on("data2", (data2) => {
+		const request = data2.toString();
 		const path = request.split(" ")[1];
-        const content = path.split("/")[2];
-        const contentLength = content.length;
+		const content = path.split("/")[2];
+		const contentLength = content.length;
 
-        socket.write("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:" + contentLength + "\r\n\r\n" + content);
+		socket.write(
+			"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:" +
+				contentLength +
+				"\r\n\r\n" +
+				content
+		);
 	});
 
 	socket.on("close", () => {
 		socket.end();
 	});
-});
 
-    socket.on("data", (data) => {
-        const request = data.toString();
+	socket.on("data", (data) => {
+		const request = data.toString();
 		const [method, path] = request.split(" ");
 		if (method === "GET" && path === "/") {
 			socket.write("HTTP/1.1 200 OK\r\n\r\n");
 		} else {
 			socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
 		}
-    })
+	});
+});
 
 server.listen(4221, "localhost");
