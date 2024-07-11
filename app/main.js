b2ef04823b20d3d9ecff6b68a8791598bfe3f20e
AlexSh1mada <alexbuerom@gmail.com>
2024-07-11 05:45:44 -0600

    lol

--- app/main.js
+++ app/main.js
@@ -5,24 +5,26 @@ console.log("Logs from your program will appear here!!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-	socket.on("data2", (data2) => {
-		const request = data2.toString();
-		const path = request.split(" ")[1];
-		const content = path.split("/")[2];
-		const contentLength = content.length;
+    socket.on("data", (data) => {
+        const request = data.toString();
+        const path = request.split(" ")[1];
 
-		socket.write(
-			"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:" +
-				contentLength +
-				"\r\n\r\n" +
-				content
-		);
-	});
+        if (path === "/") {
+            socket.write(`HTTP/1.1 200 OK\r\nContent-Length: 0\r\n\r\n`);
+        } else if (path.startsWith("/echo/")) {
+            const str = path.substring(6); // Remove '/echo/'
+            const contentLength = str.length;
+            socket.write("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:" + 
+                contentLength + "\r\n\r\n" + str);
+        } else {
+            socket.write(`HTTP/1.1 404 Not Found\r\nContent-Length: 0\r\n\r\n`);
+        }
+        socket.end();
+    });
 
-	socket.on("close", () => {
-		socket.end();
-	});
-
+    socket.on("close", () => {
+        console.log("Connection closed");
+    });
 });
 
 server.listen(4221, "localhost");
