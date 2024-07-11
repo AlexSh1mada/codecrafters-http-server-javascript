ade4a0c9d573970deb2cf1b730fec3f160fd9fd2
AlexSh1mada <alexbuerom@gmail.com>
2024-07-11 06:13:20 -0600

    Nice

--- app/main.js
+++ app/main.js
@@ -1,13 +1,13 @@
 const net = require("net");
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!!");
+console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
     socket.on("data", (data) => {
         const request = data.toString();
-        const path = request.split(" ")[1];
+        const path = request.split(" ")[1]; // path is the 'url'
 
         if (path === "/") {
             socket.write(`HTTP/1.1 200 OK\r\nContent-Length: 0\r\n\r\n`);
