51d30d58f3fae1cd9dc0443892406c5840908175
AlexSh1mada <alexbuerom@gmail.com>
2024-07-15 17:47:27 -0600

    new test

--- app/main.js
+++ app/main.js
@@ -3,6 +3,7 @@ const net = require("net");
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
+
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
     socket.on("data", (data) => {
@@ -12,10 +13,16 @@ const server = net.createServer((socket) => {
         if (path === "/") {
             socket.write(`HTTP/1.1 200 OK\r\nContent-Length: 0\r\n\r\n`);
         } else if (path.startsWith("/echo/")) {
-            const str = path.substring(6); // Remove '/echo/'
-            const contentLength = str.length;
+            const content = path.substring(6); // Remove '/echo/'
+            const contentLength = content.length;
             socket.write("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:" + 
-                contentLength + "\r\n\r\n" + str);
+                contentLength + "\r\n\r\n" + content);
+        } else if (path.startsWith('/user-agent')) {
+            const header = request.split('\r\n')[2]
+            const value = header.substring(0, 11);
+            const [ content, version ] = value.split('/') // Removes 'User-Agent:'
+            socket.write('HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ' +
+                 value.length + "\r\n\r\n" + content + '/' + version)
         } else {
             socket.write(`HTTP/1.1 404 Not Found\r\nContent-Length: 0\r\n\r\n`);
         }
