ec31f94f30d81bf33b2f7e4cebc065e8c05b22bd
AlexSh1mada <alexbuerom@gmail.com>
2024-07-15 20:03:38 -0600

    return a file

--- app/main.js
+++ app/main.js
@@ -1,4 +1,6 @@
 const net = require("net");
+const fs = require("fs");
+const pathModule = require("path");
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
@@ -10,6 +12,8 @@ const server = net.createServer((socket) => {
         const request = data.toString();
         const path = request.split(" ")[1]; // path is the 'url'
 
+        const baseDir = process.argv[2] === '--directory' ? process.argv[3] : '.'
+
         if (path === "/") {
             socket.write(`HTTP/1.1 200 OK\r\nContent-Length: 0\r\n\r\n`);
         } else if (path.startsWith("/echo/")) {
@@ -20,10 +24,10 @@ const server = net.createServer((socket) => {
                 contentLength + "\r\n\r\n" + content);
 
         } else if (path.startsWith('/user-agent')) {
-            const lines = request.split('\r\n');
+            const lines = request.split('\r\n'); // split response into lines
             let userAgent = "";
-            for (const line of lines) {
-                if (line.startsWith("User-Agent: ")) {
+            for (const line of lines) { 
+                if (line.startsWith("User-Agent: ")) { // Compare if the line starts with 'User-Agent: '
                     userAgent = line.substring(12); // Remove 'User-Agent: '
                     break;
                 }
@@ -31,6 +35,21 @@ const server = net.createServer((socket) => {
             const contentLength = userAgent.length;
             socket.write("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " +
                  contentLength + "\r\n\r\n" + userAgent);
+
+        } else if (path.startsWith('/files/')) {
+            const filename = path.substring(7) // removes '/files/' from path
+            const filepath = pathModule.join(baseDir, filename); //join base directory to filename to get full path using pathmodule for path operations
+            const contentType = 'application/octet-stream'
+
+            if (fs.existsSync(filepath)) { // Checks if file exists
+                const fileContent = fs.readFileSync(filepath) // Read file content
+                const fileSize = fileContent.length; //Get file size
+
+                socket.write(`HTTP/1.1 200 OK\r\nContent-Type: ${contentType}\r\nContent-Length: ${fileSize}\r\n\r\n`)
+                socket.write(fileContent) // Write file content response
+            } else {
+                socket.write(`HTTP/1.1 404 Not Found\r\nContent-Length: 0\r\n\r\n`);
+            }
         } else {
             socket.write(`HTTP/1.1 404 Not Found\r\nContent-Length: 0\r\n\r\n`);
         }
